f6f7cb32db29f161fa4d48b8f72a64957ddbab0e
Ethan Cheng <cshengs1994@gmail.com>
2024-04-26 16:48:55 +0800

    Revert "feat: test v-html"
    This reverts commit 43e7836965b1a532b60c9bca132d2a9092ddfd17.

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,17 +1,8 @@
 <template>
   <div>
     <h2>Contact Page</h2>
-    <p>This is the Contact Page!!</p>
-    <p v-html="rawHtml"></p>
+    <p>This is the Contact Page!</p>
   </div>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      rawHtml: '<span style="color: red">This should be red.</span>',
-    };
-  },
-};
-</script>
+<script></script>
